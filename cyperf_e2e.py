7d1dc4889b8748c6203dc0d2dc0f366ed348e1cb
Partha Majumdar <partha.majumdar@keysight.com>
2025-02-27 09:27:17 +0000

    Automatically accepting EULA.

--- cyperf_e2e.py
+++ cyperf_e2e.py
@@ -1,27 +1,103 @@
+import os
+import time
 import subprocess
 import json
 import urllib3
 import cyperf
 
+class CyPerfEULA(object):
+    def __init__(self, utils, wait_until_success=True, timeout=0):
+        self.utils              = utils
+        self.text               = ""
+        self.accepted           = False
+        self.wait_until_success = wait_until_success
+        self.wait_time          = 2
+        self.timeout            = timeout
+
+        self._read()
+
+    def url(self):
+        return f'{self.utils.host}/eula/v1/eula/CyPerf'
+
+    def _read(self):
+        import requests
+        import json
+
+        while 1:
+            try:
+                response = requests.get (self.url(), verify=False)
+                if response.ok:
+                    eula_details  = json.loads(response.content)
+                    self.accepted = eula_details['accepted']
+                    self.text     = eula_details['text']
+                    break
+                else:
+                    if not self.wait_until_success:
+                        response.raise_for_status()
+                        break
+                    else:
+                        time.sleep (self.wait_time)
+            except requests.exceptions.ConnectionError as e:
+                if not self.wait_until_success:
+                    raise (e)
+                time.sleep(self.wait_time)
+
+    def _update(self, accept=True):
+        import requests
+        import json
+
+        data     = {'accepted': accept}
+        while 1:
+            try:
+                response = requests.post (self.url(), data=json.dumps(data), verify=False)
+                if response.ok:
+                    self._read()
+                    break
+                else:
+                    if not self.wait_until_success:
+                        response.raise_for_status()
+                        break
+                    else:
+                        time.sleep (self.wait_time)
+            except requests.exceptions.ConnectionError as e:
+                if not self.wait_until_success:
+                    raise (e)
+                time.sleep(self.wait_time)
+
+    def accept(self):
+        self._update(accept=True)
+        self._read()
+
+    def reject(self):
+        self._update(accept=False)
+        self._read()
+
 class CyPerfUtils(object):
     WAP_CLIENT_ID = 'clt-wap'
 
     def __init__(self, controller, username="", password="", license_server=None, license_user="", license_password=""):
-        self.controller = controller
-        self.host = f'https://{controller}'
-        self.username = username
-        self.password = password
-        self.license_server   = license_server
-        self.license_user     = license_user
-        self.license_password = license_password
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+        self.controller               = controller
+        self.host                     = f'https://{controller}'
+        self.username                 = username
+        self.password                 = password
+        self.license_server           = license_server
+        self.license_user             = license_user
+        self.license_password         = license_password
+        self.api_ready_wait_time      = 2
 
         self.configuration            = cyperf.Configuration(host=self.host)
         self.configuration.verify_ssl = False
         self.api_client               = cyperf.ApiClient(self.configuration)
         self.added_license_servers    = []
 
+        self.eula                     = CyPerfEULA (self)
+        if not self.eula.accepted:
+            self.eula.accept()
+
         self.authorize()
-        self.update_license_server()
+        if self.license_server:
+            self.update_license_server()
 
         self.agents = {}
         agents_api  = cyperf.AgentsApi(self.api_client)
@@ -32,23 +108,29 @@ class CyPerfUtils(object):
     def __del__(self):
         self.remove_license_server()
 
+    def _call_api(self, func):
+        while 1:
+            try:
+                return func()
+            except cyperf.exceptions.ServiceException as e:
+                print(e)
+                time.sleep(self.api_ready_wait_time)
+
     def _authorize(self):
-        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
         auth_api   = cyperf.AuthorizationApi(self.api_client)
         grant_type = "password"
         try:
             response = auth_api.auth_realms_keysight_protocol_openid_connect_token_post(client_id=CyPerfUtils.WAP_CLIENT_ID,
                                                                                         grant_type=grant_type,
                                                                                         password=self.password,
-                                                                                        username=self.username)
+                                                                                        username=self.username,
+                                                                                        refresh_token='')
+            print(f'{response.access_token=}')
             return response.access_token
         except cyperf.ApiException as e:
             raise (e)
 
-    def authorize(self):
-        self.configuration.access_token = self._authorize()
-
-    def update_license_server(self):
+    def _update_license_server(self):
         if not self.license_server or self.license_server == self.controller:
             return
         license_api = cyperf.LicenseServersApi(self.api_client)
@@ -57,12 +139,12 @@ class CyPerfUtils(object):
             for lServerMetaData in response:
                 if lServerMetaData.host_name == self.license_server:
                     if 'ESTABLISHED' == lServerMetaData.connection_status:
-                        pprint(f'License server {self.license_server} is already configured')
+                        print(f'License server {self.license_server} is already configured')
                         return
                     license_api.delete_license_servers(str(lServerMetaData.id))
                     waitTime = 5 # seconds
                     print (f'Waiting for {waitTime} seconds for the license server deletion to finish.')
-                    time.sleep(5) # How can I avoid this sleep????
+                    time.sleep(waitTime) # How can I avoid this sleep????
                     break
                     
             lServer = cyperf.LicenseServerMetadata(host_name=self.license_server,
@@ -82,17 +164,26 @@ class CyPerfUtils(object):
                             print(f'Successfully added license server {s.host_name}')
                         else:
                             raise Exception(f'Could not connect to license server {s.host_name}')
-                time.sleep(0.5)
+                time.sleep(1)
         except cyperf.ApiException as e:
             raise (e)
 
-    def remove_license_server(self):
+    def _remove_license_server(self):
         license_api = cyperf.LicenseServersApi(self.api_client)
         for server in self.added_license_servers:
             try:
                 license_api.delete_license_servers(str(server.id))
             except cyperf.ApiException as e:
                 pprint(f'{e}')
+
+    def authorize(self):
+        self._call_api(self._authorize)
+
+    def update_license_server(self):
+        self._call_api(self._update_license_server)
+
+    def remove_license_server(self):
+        self._call_api(self._remove_license_server)
 
     def load_configuration_files(self, configuration_files=[]):
         config_api = cyperf.ConfigurationsApi(self.api_client)
@@ -149,6 +240,19 @@ class CyPerfUtils(object):
             self.stop_test(session)
         session_api.delete_sessions(session.id)
 
+    def delete_sessions (self, sessions=[]):
+        session_api = cyperf.SessionsApi(self.api_client)
+        for session in sessions:
+            test    = session_api.get_test (session_id = session.id)
+            if test.status != 'STOPPED':
+                self.stop_test(session)
+            session_api.delete_sessions(session.id)
+
+    def delete_all_sessions (self):
+        session_api = cyperf.SessionsApi(self.api_client)
+        result      = session_api.get_sessions()
+        # [PARTHA] TODO
+
     def assign_agents (self, session, agent_map, augment=False):
         # Assing agents to the indivual network segments based on the input provided
         for net_profile in session.config.config.network_profiles:
@@ -176,11 +280,17 @@ class CyPerfUtils(object):
 
 class Deployer(object):
     def __init__(self):
-        self.terraform_dir = './terraform'
+        self.terraform_dir             = './terraform'
+
+        self.controller_admin_user     = 'admin'
+        self.controller_admin_password = 'CyPerf&Keysight#1'
+
+        self.license_server_user       = self.controller_admin_user
+        self.license_server_password   = self.controller_admin_password
 
     def _get_utils(self, terraform_output):
         if 'mdw_detail' in terraform_output:
-            controller     = terraform_output['mdw_detail']['value']
+            controller     = terraform_output['mdw_detail']['value']['public_ip']
         else:
             controller     = None
         if 'license_server' in terraform_output:
@@ -190,6 +300,12 @@ class Deployer(object):
 
         if not controller:
             return None
+
+        print(f'{controller=}')
+        if license_server:
+            return CyPerfUtils(controller, username=self.controller_admin_user, password=self.controller_admin_password, license_server=license_server, license_user=self.license_server_user, license_password=self.license_server_password)
+        else:
+            return CyPerfUtils(controller, username=self.controller_admin_user, password=self.controller_admin_password)
 
     def terraform_deploy(self):
         # cp tfvars inside ./terraform
@@ -211,6 +327,12 @@ class Deployer(object):
         return terraform_output
 
     def terraform_destroy(self):
+        # copy tfvars inside ./terraform again, the aws key information might have changed
+        subprocess.run(['cp', 'terraform.tfvars', f'{self.terraform_dir}'], check=True)
+
+        # Initialize Terraform, in case we running destroy once more
+        subprocess.run(['terraform', f'-chdir={self.terraform_dir}',  'init'], check=True)
+
         # Destroy Terraform configuration
         subprocess.run(['terraform', f'-chdir={self.terraform_dir}', 'destroy', '-auto-approve'], check=True)
 
@@ -228,8 +350,17 @@ class Deployer(object):
         output = self.collect_terraform_output()
         utils  = self._get_utils(output)
 
+        agents = {
+            'PAN-VM-FW-Client': [output['panfw_client_agent_detail']['value']],
+            'AWS-NW-FW-Client': [output['awsfw_client_agent_detail']['value']],
+            'PAN-VM-FW-Server': [output['panfw_server_agent_detail']['value']],
+            'AWS-NW-FW-Server': [output['awsfw_server_agent_detail']['value']]
+        }
+
     def destroy(self):
         output = self.collect_terraform_output()
+        if 'license_server' in output:
+            output['license_server']['value'] = None
         utils  = self._get_utils(output)
 
         self.terraform_destroy ()
@@ -248,7 +379,6 @@ def main():
     args     = parse_cli_options()
     deployer = Deployer()
 
-    print(f'{args.deploy=}, {args.destroy=}')
     if args.deploy:
         deployer.deploy()
 
