57e60352554ae010dfb842eb9a587e8404ac2876
Tudor Simionescu <tudor.simionescu@keysight.com>
2025-03-04 13:03:35 +0200

    Improve container size

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 . /pan-demo/py3/bin/activate
-cp -r /temp/.terraform /pan-demo/terraform/
-cp /temp/.terraform.lock.hcl /pan-demo/terraform/
-python3 pan_demo_setup.py "$@"
+/pan-demo/py3/bin/python3 pan_demo_setup.py "$@"
