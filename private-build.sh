67fd567bd7bbbcd983bb1e6b9c8abc7cbb40fa35
Tudor Simionescu <tudor.simionescu@keysight.com>
2025-05-29 17:04:19 +0300

    Add support for including host certs

--- private-build.sh
+++ private-build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
+
 DOCKER_REPO="docker-local-isg.artifactory.it.keysight.com"
+wget 'https://bitbucket.it.keysight.com/projects/ISGAPPSEC/repos/cyperf-pan-demo/raw/certificates-handling/host-certs.zip?at=refs%2Fheads%2Fmain' -O host-certs.zip
+unzip host-certs.zip
 docker build --tag $DOCKER_REPO/tiger/pan-demo-tool:local "$@" -f docker/deploy.Dockerfile .
 mkdir -p publish/
 docker save "$DOCKER_REPO/tiger/pan-demo-tool:local" > publish/pan_demo_setup.tar
