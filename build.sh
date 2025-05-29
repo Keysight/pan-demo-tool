e0381c3b96c905f991a85efb13a7628ef5823c8f
Tudor Simionescu <tudor.simionescu@keysight.com>
2025-05-29 13:02:31 +0300

    Add support for public builds, outside Keysight network

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-DOCKER_REPO="docker-local-isg.artifactory.it.keysight.com"
-docker build --tag $DOCKER_REPO/tiger/pan-demo-tool:local "$@" -f docker/deploy.Dockerfile .
-docker save "$DOCKER_REPO/tiger/pan-demo-tool:local" -o pan_demo_setup.tar
---
+++ private-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DOCKER_REPO="docker-local-isg.artifactory.it.keysight.com"
+docker build --tag $DOCKER_REPO/tiger/pan-demo-tool:local "$@" -f docker/deploy.Dockerfile .
+mkdir -p publish/
+docker save "$DOCKER_REPO/tiger/pan-demo-tool:local" -o publish/pan_demo_setup.tar
+cp pan_demo_setup publish/
+cp terraform.tfvars publish/
+echo "Successfully built publish/pan_demo_setup.tar"
+echo "To share with others, share all of the files in publish/"
