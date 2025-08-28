2e89eeb5e41537bfa89f97dfb03aff4f4b7cc2c2
kabiswas <kaushik.biswas@keysight.com>
2025-08-28 19:36:39 +0530

    User define key-value pairs for aws resource tagging

--- terraform/modules/aws_agent/variables.tf
+++ terraform/modules/aws_agent/variables.tf
@@ -13,11 +13,13 @@ variable "tags" {
     aws_owner = string,
     project_tag = string,
     options_tag = string,
-    tag_ccoe-app = string,
-    tag_ccoe-group = string,
-    tag_UserID = string
   })
   description = "AWS tags"
+}
+
+variable "user_tags" {
+  type = map(string)
+  description = "dynamic key-value pairs"
 }
 
 variable "aws_stack_name" {
